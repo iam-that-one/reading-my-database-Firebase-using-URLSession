6d253346b012ef98c0c2a2f5296f40cc9116290b
iam-that-one <iam-that-one@users.noreply.github.com>
2021-04-07 20:51:03 +0300

    start

--- ReadMyDstsbase/ContentView.swift
+++ ReadMyDstsbase/ContentView.swift
@@ -13,25 +13,25 @@ struct ContentView: View {
         VStack(alignment:.leading){
             ScrollView{
                 VStack(alignment:.leading){
-                ForEach(vm.posts){ post in
-                  
-                    Image(uiImage: UIImage(data: loadImage(url1: post.imageDownloadURL) ?? Data()) ?? UIImage())
-                        .resizable()
-                        .frame(width: 400, height: 400, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    Text(post.caption)
-                }
+                    ForEach(vm.posts){ post in
+                        
+                        Image(uiImage: UIImage(data: loadImage(url1: post.imageDownloadURL) ?? Data()) ?? UIImage())
+                            .resizable()
+                            .frame(width: 400, height: 400, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Text(post.caption)
+                    }
                 }
             }
         }.onAppear{vm.loadData()}
     }
     func loadImage(url1 : String) -> Data?{
-      let url = URL(string: url1)!
-
-           // Fetch Image Data
-           if let data = try? Data(contentsOf: url) {
-               // Return Image as Data
+        let url = URL(string: url1)!
+        
+        // Fetch Image Data
+        if let data = try? Data(contentsOf: url) {
+            // Return Image as Data
             return data
-           }
+        }
         return nil
     }
 }
--- ReadMyDstsbase/ViewModel.swift
+++ ReadMyDstsbase/ViewModel.swift
@@ -43,7 +43,6 @@ class ViewModel: ObservableObject{
             
         }.resume()
         
-        
     }
 }
 
